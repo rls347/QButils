b92af353c9242d9e01252fa4312530a96ca3aa9c
Rachel Storer <rls347@gmail.com>
2019-02-21 15:08:53 -0800

    add capability for coloring by another value

--- plots.py
+++ plots.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib.collections import LineCollection
 from rachelutils.hdfload import getvar
 
-def plotprofs(profiles, filename):
+def plotprofs(profiles, filename, colorvar=None):
     height = getvar('/home/rstorer/aug11-control-revu-020.h5','z_coords')/1000.
     plotvars = []
     for var in profiles:
@@ -16,18 +16,16 @@ def plotprofs(profiles, filename):
     stdvar = np.nanstd(vararray,0)
     lines = [zip(x,y) for x, y in plotvars]
     fig, ax = plt.subplots()
-    lines1 = LineCollection(lines, linewidth=1)
-    ax.add_collection(lines1)
+    if colorvar is None:
+        lines1 = LineCollection(lines, linewidth=1)
+        ax.add_collection(lines1)
+    else:
+        lines1=LineCollection(lines,array = colorvar, cmap = plt.cm.plasma,linewidth=2)
+        ax.add_collection(lines1)
     plt.plot(meanvar,height,color='black',linewidth=2)
     ax.set_ylim(0,18)
     ax.set_xlim(-35,45)
     plt.savefig(filename)
-    plt.clf()
-
-    plt.plot(stdvar,height,color='black',linewidth=2)
-    filout = filename[:-4]+'-stdev.png'
-    plt.xlim(0,5.5)
-    plt.savefig(filout)
     plt.clf()
 
     return
